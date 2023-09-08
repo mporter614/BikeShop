e977890f0fc555f6af77ebc6785636a383d53da1
Mark Porter <m.porter614@gmail.com>
2023-09-07 22:09:16 -0700

    Fix bike shop module forgotten import of its routing module

--- bike-shop/src/app/bike-shop/bike-shop.module.ts
+++ bike-shop/src/app/bike-shop/bike-shop.module.ts
@@ -2,16 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BikeListComponent } from './bike-list/bike-list.component';
 import { BikeComponent } from './bike/bike.component';
-
-
+import { BikeShopRoutingModule } from './bike-shop-routing.module';
 
 @NgModule({
-  declarations: [
-    BikeListComponent,
-    BikeComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [BikeListComponent, BikeComponent],
+  imports: [CommonModule, BikeShopRoutingModule],
 })
-export class BikeShopModule { }
+export class BikeShopModule {}
