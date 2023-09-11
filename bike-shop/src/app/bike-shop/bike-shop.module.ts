063899a420f624420e8d9fa60d041a1854962d45
Mark Porter <m.porter614@gmail.com>
2023-09-11 00:43:36 -0700

    Add some rough styling and angular material components to app component, bike list

--- bike-shop/src/app/bike-shop/bike-shop.module.ts
+++ bike-shop/src/app/bike-shop/bike-shop.module.ts
@@ -3,9 +3,18 @@ import { CommonModule } from '@angular/common';
 import { BikeListComponent } from './bike-list/bike-list.component';
 import { BikeComponent } from './bike/bike.component';
 import { BikeShopRoutingModule } from './bike-shop-routing.module';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
+import { MatDividerModule } from '@angular/material/divider';
 
 @NgModule({
   declarations: [BikeListComponent, BikeComponent],
-  imports: [CommonModule, BikeShopRoutingModule],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatListModule,
+    MatDividerModule,
+    BikeShopRoutingModule,
+  ],
 })
 export class BikeShopModule {}
