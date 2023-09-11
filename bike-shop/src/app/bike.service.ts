b3d1f0ce0f22b89d4cf997d9908cf017b138a8df
Mark Porter <m.porter614@gmail.com>
2023-09-10 23:11:24 -0700

    Generate bike service + add rough drafts of all methods to implement for CRUD functions

---
+++ bike-shop/src/app/bike.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class BikeService {
+  constructor() {}
+
+  getAll() {}
+
+  get(id: string) {}
+
+  create() {}
+
+  update(id: string) {}
+
+  delete(id: string) {}
+}
