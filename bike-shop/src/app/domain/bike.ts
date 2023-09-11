07c6b53e28a331d01cc96b2028c90520d0cf950e
Mark Porter <m.porter614@gmail.com>
2023-09-10 23:10:20 -0700

    Add bike json stub data for proofing out bike service

--- bike-shop/src/app/domain/bike.ts
+++ bike-shop/src/app/domain/bike.ts
@@ -1,5 +1,6 @@
 export interface Bike {
   id: string;
+  name: string;
   description: string;
   price: number;
   quantity: number;
@@ -8,11 +9,12 @@ export interface Bike {
   photoUrl: string;
 }
 
+//Likely driven from theoretical API values, specifically in this example project numbering these here for ease in our .json stubbed data in our case
 export enum BikeType {
-  Mountain,
-  Street,
-  BMX,
-  Kids,
-  Tandem,
-  Pennyfarthing,
+  Mountain = 0,
+  Street = 1,
+  BMX = 2,
+  Kids = 3,
+  Tandem = 4,
+  Pennyfarthing = 5,
 }
