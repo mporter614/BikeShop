ddbbf59874b1e14a2e139091bb8697a80d43b11b
Mark Porter <m.porter614@gmail.com>
2023-09-12 14:32:27 -0700

    Add rest of desired fields to bike form

--- bike-shop/src/app/bike-shop/bike/bike.component.ts
+++ bike-shop/src/app/bike-shop/bike/bike.component.ts
@@ -12,6 +12,9 @@ import { Bike, BikeType } from 'src/app/domain/bike';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class BikeComponent implements OnInit {
+  bikeType = BikeType;
+  typeEnumKeys: number[];
+
   bikeForm = this.formBuilder.group({
     name: ['', Validators.required],
     description: ['', Validators.required],
@@ -29,7 +32,14 @@ export class BikeComponent implements OnInit {
     private formBuilder: FormBuilder,
     private route: ActivatedRoute,
     private bikeService: BikeService
-  ) {}
+  ) {
+    console.log(Object.keys(this.bikeType));
+    this.typeEnumKeys = Object.keys(this.bikeType)
+      .filter((f) => !isNaN(Number(f)))
+      .map(Number);
+    //this.enumKeys = this.enumKeys.map(key => Number(key));
+    console.log(this.typeEnumKeys);
+  }
 
   ngOnInit(): void {
     //Experiment to simplify the form group definitions / not bog them down with initialization
@@ -40,12 +50,13 @@ export class BikeComponent implements OnInit {
       .get(bikeId ?? '')
       .pipe(
         //Side effect from the service bike data not being ready when accessing individual bike page (refresh or direct URL access) that undefined is emitted
-        //Ideally fix this when moving to consuming actual API / or state management pattern
+        //Ideally fix this when moving to consuming actual API / or state management pattern - BehaviorSubject initial value is root cause
         filter((res) => res !== undefined),
         tap((bike) => {
           console.log(bike);
           this.bikeData = bike ?? null;
           this.bikeForm.patchValue({ ...this.bikeData });
+          //this.selectedBikeType = this.bikeData?.type;
         })
       )
       .subscribe();
@@ -54,5 +65,17 @@ export class BikeComponent implements OnInit {
   onSubmit() {
     // TODO: emit + consume form data in parent component
     console.log(this.bikeForm.value);
+  }
+
+  onCreate() {
+    console.log('onCreate clicked');
+  }
+
+  onUpdate() {
+    console.log('onUpdate clicked');
+  }
+
+  onDelete() {
+    console.log('onDelete clicked');
   }
 }
