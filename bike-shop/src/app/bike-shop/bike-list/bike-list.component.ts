f9a3230ff84040ca54a1ac1d6eebaac39ff48f36
Mark Porter <m.porter614@gmail.com>
2023-09-12 10:08:50 -0700

    Opt into onpush change detection for bike components

--- bike-shop/src/app/bike-shop/bike-list/bike-list.component.ts
+++ bike-shop/src/app/bike-shop/bike-list/bike-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { Bike, BikeType } from '../../domain/bike';
 import { BikeService } from 'src/app/bike.service';
 import { Observable } from 'rxjs';
@@ -6,6 +6,7 @@ import { Observable } from 'rxjs';
   selector: 'app-bike-list',
   templateUrl: './bike-list.component.html',
   styleUrls: ['./bike-list.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class BikeListComponent {
   bikes$: Observable<Bike[]> = this.bikeService.getAll();
