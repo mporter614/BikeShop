7e503f64fe469039d893e20b20b7f3891e046835
Mark Porter <m.porter614@gmail.com>
2023-09-13 07:48:33 -0700

    Remove unused configurable external api widget component from shared module (functionality was better fit for a service to consume in our client's components)

--- bike-shop/src/app/shared/shared.module.ts
+++ bike-shop/src/app/shared/shared.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ConfigurableExternalApiWidgetComponent } from './configurable-external-api-widget/configurable-external-api-widget.component';
 
 @NgModule({
-  declarations: [ConfigurableExternalApiWidgetComponent],
   imports: [CommonModule],
 })
 export class SharedModule {}
